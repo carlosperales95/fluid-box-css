1dae80fdd857a5ce5cdde3690e8fb07f94bcbde3
carlosperales95 <carlos.perales95@opendeusto.es>
2023-03-05 00:54:04 +0100

    draft upload

---
+++ fluid-box/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="mainBox">
+      <div class="smallBox">
+      
+      </div>
+      <h1><b>Fluid Box Anim</b></h1>
+      <p>Put your content here</p>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
+
